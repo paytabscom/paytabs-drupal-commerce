e8775ffc75538e04138c420720c352a178fa056f
Walaa Elsaeed <79043296+Walaa-Elsaeed-93@users.noreply.github.com>
2023-02-22 19:25:41 +0200

    add palestine

--- src/Plugin/Commerce/PaymentGateway/PaytabsOffsiteRedirect.php
+++ src/Plugin/Commerce/PaymentGateway/PaytabsOffsiteRedirect.php
@@ -175,6 +175,7 @@ class PaytabsOffsiteRedirect extends OffsitePaymentGatewayBase implements Suppor
                 'OMN' => $this->t('Oman'),
                 'JOR' => $this->t('Jordan'),
                 'IRQ' => $this->t('Iraq'),
+                'PSE' => $this->t('Palestine'),
                 'GLOBAL' => $this->t('GLOBAL'),
             ],
             '#default_value' => $this->configuration['region'],
